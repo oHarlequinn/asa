da8db120f7cf1cf21d4468a19b187dc8be6a10d0
oHarlequinn <186018336+oHarlequinn@users.noreply.github.com>
2024-11-22 12:20:58 +0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+
+import Layout from './components/Layout';
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Resource from './pages/Resource';
+import Social from './pages/Social'
+import Wuwa from './pages/Wuwa'
+
+
+function App() {
+  return (
+    <Router>
+      {/* <Header></Header> */}
+      <Layout>
+
+        <Routes>
+          {/* Render home as default */}
+          <Route path="/" element={<Home />} />
+          {/* Secondary pages */}
+          <Route path="/about" element={<About />} />
+          <Route path="/resource" element={<Resource />} />
+          <Route path="/social" element={<Social />} />
+          <Route path="/wuwa" element={<Wuwa />} />
+        </Routes>
+
+      </Layout>
+      {/* <Footer></Footer> */}
+    </Router>
+  );
+}
+
+export default App;
